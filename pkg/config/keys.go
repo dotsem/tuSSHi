82bca5421b7f69e310e47436a6bacd5678c7d048
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-05-24 21:17:27 +0200

    fix goconst

---
+++ pkg/config/keys.go
@@ -0,0 +1,10 @@
+package config
+
+const (
+	keyHostName     = "HostName"
+	keyUser         = "User"
+	keyPort         = "Port"
+	keyIdentityFile = "IdentityFile"
+	keyForwardAgent = "ForwardAgent"
+	keyProxyJump    = "ProxyJump"
+)
